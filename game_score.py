edeb867edb839d4be36b3a90b35453da0330d5f6
unknown <Punyashreeks>
2026-06-22 00:03:39 +0530

    Added game score calculator

---
+++ game_score.py
@@ -0,0 +1,10 @@
+print("Game Score Calculator")
+
+score = int(input("Enter Score: "))
+
+if score >= 80:
+    print("Excellent")
+elif score >= 50:
+    print("Good")
+else:
+    print("Practice More")
